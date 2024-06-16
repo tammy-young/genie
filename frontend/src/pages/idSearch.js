30bdbae849f053bd2b6e034b75cb0067b40d62bd
Tammy Young <tammy.q.young@gmail.com>
2024-06-15 23:31:25 -0400

    feat: make id search table prettier

--- frontend/src/pages/idSearch.js
+++ frontend/src/pages/idSearch.js
@@ -4,7 +4,35 @@ import FormLabel from '@mui/joy/FormLabel';
 import constants from '../constants';
 import './../App.css';
 import { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: constants.colors.PRIMARY,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
 
 const reset = () => {
     let allBrandsDiv = document.getElementById(constants.divIds.BRANDS_ID_TO_NAME);
@@ -25,7 +53,6 @@ const IdSearch = () => {
 
     const updateSearchingBrand = (brand) => {
         setSearchingFor(brand.target.value);
-        console.log("searching for " + brand);
     }
 
     useEffect(() => {
@@ -42,32 +69,41 @@ const IdSearch = () => {
                          onChange={ updateSearchingBrand } />
                     </FormControl>
                 </div>
-                <div className="col" style={{ overflowY: "scroll", maxWidth: '100%', maxHeight: '85vh' }}>
-                    <div className="row">
-                        <div className="col">
-                            Name
-                        </div>
-                        <div className="col">
-                            Id
-                        </div>
-                    </div>
-                    { searchedBrands
-                        .filter(brand => {
-                            const searchedBrand = searchingFor.toLowerCase();
-                            const brandName = brand.toLowerCase();
-                            return (searchedBrand && brandName.includes(searchedBrand) && brandName !== searchedBrand) || searchedBrand === "";
-                        })
-                        .map((brand) => (
-                            <div className='row id-search-row' onClick={() => updateSearchingBrand(brand)} key={brand}>
-                                <div className="col">
-                                    { brand }
-                                </div>
-                                <div className="col">
-                                    { getIdFromBrandName(brand) }
-                                </div>
-                            </div>)
-                        )
-                    }
+                <div className="col scrollable" style={{ maxWidth: '100%', maxHeight: '84vh' }}>
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell>
+                                        Name
+                                    </StyledTableCell>
+                                    <StyledTableCell>
+                                        Id
+                                    </StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody style={{ maxHeight: '79vh' }}>
+                                { searchedBrands
+                                    .filter(brand => {
+                                        const searchedBrand = searchingFor.toLowerCase();
+                                        const brandName = brand.toLowerCase();
+                                        return (searchedBrand && brandName.includes(searchedBrand) && brandName !== searchedBrand) || searchedBrand === "";
+                                    })
+                                    .map((brand) => (
+                                        <StyledTableRow>
+                                            <StyledTableCell>
+                                                { brand }
+                                            </StyledTableCell>
+                                            <StyledTableCell>
+                                                { getIdFromBrandName(brand) }
+                                            </StyledTableCell>
+                                        </StyledTableRow>
+                                        )
+                                    )
+                                }
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </div>
             </div>
         </div>
