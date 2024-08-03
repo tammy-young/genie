6a9917d740767f62fb5859213ba46b4e33645635
Tammy Young <tammy.q.young@gmail.com>
2024-08-03 15:58:27 -0400

    feat: add active tab on nav bar

--- frontend/src/components/navBar.js
+++ frontend/src/components/navBar.js
@@ -1,23 +1,33 @@
 import SearchIcon from '@mui/icons-material/Search';
 import NumbersIcon from '@mui/icons-material/Numbers';
+import { NavLink, useLocation } from 'react-router-dom';
+
 import GenieIcon from './images/genieLogo.js';
 import constants from '../constants.js';
+import "../App.css";
+
 
 const NavBar = () => {
+    const location = useLocation();
+
+    const isActive = (path) => {
+        return location.pathname === path;
+    }
+
     return (
-        <div className='row'>
+        <div className='row nav-bar'>
             <div className='col' style={{ padding: '0px', textAlign: 'left', width: 'fit-content', maxWidth: 'fit-content' }}>
                 <GenieIcon />
             </div>
-            <div className='col' style={{ display: 'flex', alignItems: 'center', maxWidth: 'fit-content' }} data-nav-id={ 0 }>
-                <div onClick={() => window.location.href = constants.paths.SEARCH } style={{ cursor: 'pointer' }}>
+            <div className='col nav' style={{ display: 'flex', alignItems: 'center', maxWidth: 'fit-content' }} >
+                <NavLink to={ constants.paths.SEARCH } className={"nav-link" + (isActive(constants.paths.SEARCH) ? " selected" : "")} >
                     Search <SearchIcon />
-                </div>
+                </NavLink>
             </div>
-            <div className='col' style={{ display: 'flex', alignItems: 'center', maxWidth: 'fit-content' }} data-nav-id={ 0 }>
-                <div onClick={() => window.location.href = constants.paths.ID_SEARCH } style={{ cursor: 'pointer' }}>
-                    Id Number Search <NumbersIcon />
-                </div>
+            <div className='col nav' style={{ display: 'flex', alignItems: 'center', maxWidth: 'fit-content' }} >
+                <NavLink to={ constants.paths.ID_SEARCH } className={"nav-link" + (isActive(constants.paths.ID_SEARCH) ? " selected" : "")} >
+                    Brands <NumbersIcon />
+                </NavLink>
             </div>
             <div className='col'></div>
         </div>
