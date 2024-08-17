dd7035666b1582c96d045a99873ce1899dbd3127
Tammy Young <tammy.q.young@gmail.com>
2024-08-17 16:50:01 -0400

    feat: search netlify function

---
+++ backend/functions/search.js
@@ -0,0 +1,119 @@
+import express from "express";
+import cors from 'cors';
+import 'dotenv/config';
+import ServerlessHttp from "serverless-http";
+
+
+const ITEMS_KEY = "items";
+const ITEM_INFO = ["brand", "name", "currencyType", "originalPrice", "sellPrice", "sellerId"];
+const BAZAAR_URL = "https://www.stardoll.com/en/com/user/getStarBazaar.php";
+const SEARCH_URL_PART = "?search&type=fashion&Price=24";
+const MAX_ITEMS_AT_ONCE = 20;
+
+const itemImageUrl = (id) => { return `http://cdn.stardoll.com/itemimages/76/0/98/${id}.png` };
+
+async function fetchData(url, html=false) {
+    try {
+        const response = await fetch(url, {
+            withCredentials: true,
+            headers: {
+                Cookie: "pdhUser=" + process.env.PDH_USER + ";"
+            }
+        });
+        const data = html? response.text() : response.json();
+        return data;
+    } catch (e) {
+        console.error("Could not get response:", e);
+    }
+}
+
+const getItemInfo = async (item) => {
+    let itemId = item['itemId'];
+    let itemInfo = {};
+    ITEM_INFO.forEach(info => {
+        itemInfo[info] = typeof item[info] === "string"? item[info].replace(/&amp;/g, "&") : item[info];
+    })
+    itemInfo["itemImage"] = itemImageUrl(itemId);
+    return itemInfo;
+}
+
+const search = async (req) => {
+
+    let searchUrl = BAZAAR_URL + SEARCH_URL_PART;
+    let itemName = req.query.itemName.toLowerCase();
+
+    let brandId = req.query.brandId;
+    searchUrl += brandId !== ""? `&brands=${brandId}` : "";
+
+    let minPrice = req.query.minPrice;
+    searchUrl += minPrice !== ""? `&minPrice=${minPrice}` : "";
+
+    let maxPrice = req.query.maxPrice;
+    searchUrl += maxPrice !== ""? `&maxPrice=${maxPrice}` : "";
+
+    let currencyType = req.query.currencyType;
+    searchUrl += (minPrice || maxPrice) && (currencyType !== "")? `&currencyType=${currencyType}` : "";
+
+    let showStardesign = (req.query.showStardesign === 'true');
+
+    let items = [];
+    let itemIds = [];
+    let stopSearchTime = Date.now() + 10000;
+
+    while (Date.now() < stopSearchTime && items.length < MAX_ITEMS_AT_ONCE) {
+        let returnedPage = await fetchData(searchUrl);
+
+        // if there are no items on the page then get a new page
+        if (returnedPage != null && ITEMS_KEY in returnedPage) {
+            let returned_items = returnedPage[ITEMS_KEY];
+
+            for (let item of returned_items) {
+                let itemId = item['itemId'];
+                let addItem = false;
+
+                if (!showStardesign && item.brand == "43") {
+                    continue;
+                }
+
+                if (itemName !== "") {
+                    let searchedItemName = item.name.toLowerCase();
+                    if (searchedItemName.includes(itemName)) {
+                        addItem = true;
+                    }
+                } else {
+                    if (!itemIds.includes(itemId)) {
+                        addItem = true;
+                        itemIds.push(itemId);
+                    }
+                }
+                    
+                if (addItem) {
+                    let itemInfo = await getItemInfo(item);
+                    items.push(itemInfo);
+                }
+
+                if (items.length >= MAX_ITEMS_AT_ONCE) {
+                    break;
+                }
+            }
+        }
+    }
+
+    return {"items": items};
+}
+
+const app = express();
+app.use(cors())
+
+app.get("/.netlify/functions/search", async (req, res) => {
+    search(req)
+    .then(data => {
+        res.json(data);
+    });
+});
+
+const handler = ServerlessHttp(app);
+module.exports.handler = async(event, context) => {
+    const result = handler(event, context);
+    return result;
+}
