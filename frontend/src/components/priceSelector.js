d3c7eddfad469c2433f39bdae72d26b4d89a19f6
Tammy Young <tammy.q.young@gmail.com>
2025-01-19 02:08:44 -0700

    fix: can't select price range when no currency type preferred

--- frontend/src/components/priceSelector.js
+++ frontend/src/components/priceSelector.js
@@ -36,55 +36,56 @@ const PriceSelector = () => {
         setValue([minPriceInputValue, maxPriceInputValue]);
     }
 
-    return(
+    return (
         <>
-            <div className='row'>
-                <div className='col'>
-                    Price Range
+            <div className={`flex flex-col row px-2 ${selectedBtn === 0 ? "!hidden" : ""}`}>
+                <div className='row'>
+                    <div className='col'>
+                        Price Range
+                    </div>
                 </div>
-            </div>
-            <div className="row">
                 <div className='col'>
-                    <ThemeProvider theme={ theme }>
-                        <Slider getAriaLabel={() => 'Starcoins'} value={ value } onChange={ handleChange } valueLabelDisplay="auto" color="primary"
-                            getAriaValueText={ valuetext } min={ defaultMin } max={ defaultMax } id={ constants.filterValuesIds.FASHION_PRICE }/>
+                    <ThemeProvider theme={theme}>
+                        <Slider getAriaLabel={() => 'Starcoins'} value={value} onChange={handleChange} valueLabelDisplay="auto" color="primary"
+                            getAriaValueText={valuetext} min={defaultMin} max={defaultMax} id={constants.filterValuesIds.FASHION_PRICE} />
                     </ThemeProvider>
                 </div>
             </div>
-            <div className="row">
-                <div className='col'>
+
+            <div className="flex flex-row justify-between">
+                <div className={`flex sm:flex-col ${selectedBtn === 0 ? "hidden" : ""}`}>
                     <label style={{ paddingRight: '10px' }}>
-                        Min<br/>
-                        <input type="number" id={ constants.filterValuesIds.FASHION_MIN_PRICE } min={ defaultMin } max={ defaultMax }
-                            onChange={ priceRangeChangeFromInput } defaultValue={ defaultMin } className='my-input' />
+                        Min<br />
+                        <input type="number" id={constants.filterValuesIds.FASHION_MIN_PRICE} min={defaultMin} max={defaultMax}
+                            onChange={priceRangeChangeFromInput} defaultValue={defaultMin} className='my-input' />
                     </label>
                     <label>
-                        Max<br/>
-                        <input type="number" id={ constants.filterValuesIds.FASHION_MAX_PRICE } min={ defaultMin } max={ defaultMax }
-                            onChange={ priceRangeChangeFromInput } defaultValue={ defaultMax } className='my-input' />
+                        Max<br />
+                        <input type="number" id={constants.filterValuesIds.FASHION_MAX_PRICE} min={defaultMin} max={defaultMax}
+                            onChange={priceRangeChangeFromInput} defaultValue={defaultMax} className='my-input' />
                     </label>
                 </div>
-                <div className="col">
+                <div className="">
                     Currency Type
                     <div>
-                        <ThemeProvider theme={ theme }>
+                        <ThemeProvider theme={theme}>
                             <ButtonGroup variant="outlined" aria-label="Currency Type Select">
                                 <Button
-                                 className={selectedBtn === 0 ? constants.filterValuesIds.SELECTED_CURRENCY : ""} id="0"
-                                 color={selectedBtn === 0 ? "primary" : "secondary"} style={{ color: constants.colors.LIGHT_GREY, fontWeight: "normal" }}
-                                 variant={selectedBtn === 0 ? "contained" : "outlined"} onClick={()=>setSelectedBtn(0)}>
+                                    className={selectedBtn === 0 ? constants.filterValuesIds.SELECTED_CURRENCY : ""} id="0"
+                                    color={selectedBtn === 0 ? "primary" : "secondary"} style={{ color: constants.colors.LIGHT_GREY, fontWeight: "normal" }}
+                                    variant={selectedBtn === 0 ? "contained" : "outlined"} onClick={() => setSelectedBtn(0)}>
                                     N/A
                                 </Button>
                                 <Button
-                                 className={selectedBtn === 1 ? constants.filterValuesIds.SELECTED_CURRENCY : ""} id="1"
-                                 color={selectedBtn === 1 ? "primary" : "secondary"}
-                                 variant={selectedBtn === 1 ? "contained" : "outlined"} onClick={()=>setSelectedBtn(1)}>
+                                    className={selectedBtn === 1 ? constants.filterValuesIds.SELECTED_CURRENCY : ""} id="1"
+                                    color={selectedBtn === 1 ? "primary" : "secondary"}
+                                    variant={selectedBtn === 1 ? "contained" : "outlined"} onClick={() => setSelectedBtn(1)}>
                                     <StardollarIcon />
                                 </Button>
                                 <Button
-                                 className={selectedBtn === 2 ? constants.filterValuesIds.SELECTED_CURRENCY : ""} id="2"
-                                 color={selectedBtn === 2 ? "primary" : "secondary"}
-                                 variant={selectedBtn === 2 ? "contained" : "outlined"} onClick={()=>setSelectedBtn(2)}>
+                                    className={selectedBtn === 2 ? constants.filterValuesIds.SELECTED_CURRENCY : ""} id="2"
+                                    color={selectedBtn === 2 ? "primary" : "secondary"}
+                                    variant={selectedBtn === 2 ? "contained" : "outlined"} onClick={() => setSelectedBtn(2)}>
                                     <StarcoinIcon />
                                 </Button>
                             </ButtonGroup>
