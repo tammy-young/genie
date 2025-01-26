7ed9e4311c159e82a5b8c30e19029f7f53a55969
Tammy Young <tammy.q.young@gmail.com>
2025-01-26 02:50:47 -0700

    feat: make nav bar takes up less space on mobile

--- frontend/src/components/navBar.js
+++ frontend/src/components/navBar.js
@@ -6,6 +6,7 @@ import CoffeeTwoToneIcon from '@mui/icons-material/CoffeeTwoTone';
 
 import GenieLogo from './images/genieLogo.js';
 import constants from '../constants.js';
+import GenieIcon from './images/genieIcon.js';
 
 
 const NavBar = () => {
@@ -17,35 +18,43 @@ const NavBar = () => {
 
   return (
     location.pathname !== "" ? (
-      <div className='nav-bar flex flex-col sm:flex-row flex-wrap space-y-3 sm:space-y-0 sm:justify-between overflow-auto'>
-        <div className='flex flex-row justify-center sm:justify-start p-0'>
+      <div className='nav-bar flex flex-row flex-wrap space-y-3 sm:space-y-0 justify-between overflow-auto'>
+
+        <div className='flex flex-row justify-center sm:justify-start p-0 sm:block hidden'>
           <NavLink to="/">
             <GenieLogo />
           </NavLink>
         </div>
-        <div className='flex justify-center space-x-3'>
+        <div className='p-0 sm:hidden block w-fit'>
+          <NavLink to="/">
+            <GenieIcon />
+          </NavLink>
+        </div>
+
+        <div className='flex sm:justify-center space-x-3 sm:w-max w-fit'>
           <div className='nav' style={{ display: 'flex', alignItems: 'center', maxWidth: 'fit-content', paddingRight: '0px' }} >
             <NavLink to={constants.paths.FASHION} className={"nav-link" + (isActive(constants.paths.FASHION) ? " fashion-selected" : "")} style={{ display: 'flex' }}>
-              <div>Fashion</div>
+              <div className='sm:block hidden'>Fashion</div>
               <div><CheckroomIcon /></div>
             </NavLink>
           </div>
           <div className='nav' style={{ display: 'flex', alignItems: 'center', maxWidth: 'fit-content', paddingRight: '0px' }} >
             <NavLink to={constants.paths.INTERIOR} className={"nav-link" + (isActive(constants.paths.INTERIOR) ? " interior-selected" : "")} style={{ display: 'flex' }}>
-              <div>Interior</div>
+              <div className='sm:block hidden'>Interior</div>
               <div><ChairIcon /></div>
             </NavLink>
           </div>
           <div className='nav' style={{ display: 'flex', alignItems: 'center', maxWidth: 'fit-content' }} >
             <NavLink to={constants.paths.ID_SEARCH} className={"nav-link" + (isActive(constants.paths.ID_SEARCH) ? " selected" : "")} style={{ display: 'flex' }}>
-              Brands <div><NumbersIcon /></div>
+              <div className='sm:block hidden'>Brands</div>
+              <div><NumbersIcon /></div>
             </NavLink>
           </div>
         </div>
-        <div className='flex items-center w-full md:w-fit justify-center sm:justify-end'>
+        <div className='flex items-center sm:w-max md:w-fit justify-center sm:justify-end'>
           <a href='https://buymeacoffee.com/anastaciasd' target='_blank' rel='noreferrer' className='hover:text-white hover:!no-underline'>
             <button className='!bg-primary p-2 rounded text-white flex flex-row space-x-1 items-center'>
-              <p className='mb-0'>Support Genie</p>
+              <p className='mb-0 md:block hidden'>Support Genie</p>
               <CoffeeTwoToneIcon />
             </button>
           </a>
