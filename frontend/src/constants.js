44c518eac7d604e43761b89df89149859d3b093f
Tammy Young <tammy.q.young@gmail.com>
2025-01-31 17:57:57 -0700

    fix: api url in constants file

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -47,7 +47,7 @@ const constants = {
         ID_SEARCH: "/brands"
     },
     backend: {
-        API: "http://localhost:8888",
+        API: "https://genie-api.netlify.app",
         GET_BRANDS: "/.netlify/functions/getBrands",
         SEARCH: "/.netlify/functions/search",
         GET_SELLER: "/.netlify/functions/getSeller"
