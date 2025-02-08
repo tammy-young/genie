23149680f7d3097d7bfd356ef73e77a8243d1b7f
Tammy Young <tammy.q.young@gmail.com>
2025-02-07 17:21:50 -0700

    feat: make logo smaller

--- frontend/src/components/images/genieLogo.js
+++ frontend/src/components/images/genieLogo.js
@@ -2,7 +2,7 @@ import "./../../App.css";
 
 const GenieLogo = () => {
     return(
-        <img className="app-logo sm:min-w-[182px] !h-20 sm:!h-[100px]" src={ process.env.PUBLIC_URL + "genie-logo.png"} alt="Genie"></img>
+        <img className="app-logo sm:min-w-[150px] sm:!h-20 !h-16" src={ process.env.PUBLIC_URL + "genie-logo.png"} alt="Genie"></img>
     );
 }
 
