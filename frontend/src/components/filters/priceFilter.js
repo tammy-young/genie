b56d215bb34b07e73f74e643136ca6fadaf8fff8
Tammy Young <tammy.q.young@gmail.com>
2025-02-07 17:16:48 -0700

    feat: remove price slider

--- frontend/src/components/filters/priceFilter.js
+++ frontend/src/components/filters/priceFilter.js
@@ -4,7 +4,6 @@ import StardollarIcon from '../images/stardollar';
 import StarcoinIcon from '../images/starcoin';
 
 import ButtonGroup from '@mui/joy/ButtonGroup';
-import Slider from '@mui/joy/Slider';
 import Button from '@mui/joy/Button';
 import FormLabel from '@mui/joy/FormLabel';
 import FormControl from '@mui/joy/FormControl';
@@ -13,17 +12,9 @@ import FormControl from '@mui/joy/FormControl';
 const defaultMin = 2;
 const defaultMax = 600;
 
-function valuetext(value) {
-  return `${value}°C`;
-}
-
 const PriceSelector = ({ setPriceRange, setCurrencyType, priceRange, currencyType }) => {
   const [value, setValue] = useState([defaultMin, defaultMax]);
   const [selectedBtn, setSelectedBtn] = useState(0);
-
-  const handleSliderChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
   const handleMinInputChange = (event) => {
     const newMin = Math.min(Number(event.target.value), value[1]);
@@ -47,56 +38,41 @@ const PriceSelector = ({ setPriceRange, setCurrencyType, priceRange, currencyTyp
   }, [currencyType]);
 
   return (
-    <div className='flex flex-col space-y-2'>
-      <div className="flex flex-row space-x-8">
-        <FormControl>
-          <FormLabel>Currency Type</FormLabel>
-          <ButtonGroup variant="outlined" aria-label="Currency Type Select">
-            <Button
-              className={selectedBtn === 0 ? constants.filterValuesIds.SELECTED_CURRENCY + " !bg-primary" : " !bg-neutral-100 dark:!bg-neutral-800 dark:!text-white"} id="0"
-              style={{ fontWeight: "normal" }}
-              variant={selectedBtn === 0 ? "soft" : ""} onClick={() => changeCurrencyType(0)}>
-              N/A
-            </Button>
-            <Button
-              className={selectedBtn === 1 ? constants.filterValuesIds.SELECTED_CURRENCY + " !bg-primary" : " !bg-neutral-100 dark:!bg-neutral-800"} id="1"
-              variant={selectedBtn === 1 ? "soft" : ""} onClick={() => changeCurrencyType(1)}>
-              <StardollarIcon />
-            </Button>
-            <Button
-              className={selectedBtn === 2 ? constants.filterValuesIds.SELECTED_CURRENCY + " !bg-primary" : " !bg-neutral-100 dark:!bg-neutral-800"} id="2"
-              variant={selectedBtn === 2 ? "soft" : ""} onClick={() => changeCurrencyType(2)}>
-              <StarcoinIcon />
-            </Button>
-          </ButtonGroup>
-        </FormControl>
-
-        <div className={`flex flex-row space-x-2 ${selectedBtn === 0 ? "hidden" : ""}`}>
-          <FormControl>
-            <FormLabel>Min</FormLabel>
-            <input type="number" id={constants.filterValuesIds.FASHION_MIN_PRICE} min={defaultMin} max={defaultMax}
-              onChange={handleMinInputChange} value={value[0]} className='my-input font-normal h-9 w-[4.5rem] text-clip' />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Max</FormLabel>
-            <input type="number" id={constants.filterValuesIds.FASHION_MAX_PRICE} min={defaultMin} max={defaultMax}
-              onChange={handleMaxInputChange} value={value[1]} className='my-input font-normal h-9 w-[4.5rem]' />
-          </FormControl>
-        </div>
-      </div>
-
-      <FormControl className={`flex flex-col row ${selectedBtn === 0 ? "!hidden" : ""} ml-0 mr-2`}>
-        <FormLabel>Price Range</FormLabel>
-        <div className='pl-2'>
-          <Slider getAriaLabel={() => 'Starcoins'} value={priceRange} onChange={handleSliderChange} valueLabelDisplay="auto"
-            color='neutral'
-            className="pb-0"
-            onChangeCommitted={(e, val) => {
-              setPriceRange(val);
-            }}
-            getAriaValueText={valuetext} min={defaultMin} max={defaultMax} id={constants.filterValuesIds.FASHION_PRICE} />
-        </div>
+    <div className="flex flex-row space-x-8">
+      <FormControl>
+        <FormLabel>Currency Type</FormLabel>
+        <ButtonGroup variant="outlined" aria-label="Currency Type Select">
+          <Button
+            className={selectedBtn === 0 ? constants.filterValuesIds.SELECTED_CURRENCY + " !bg-primary" : " !bg-neutral-100 dark:!bg-neutral-800 dark:!text-white"} id="0"
+            style={{ fontWeight: "normal" }}
+            variant={selectedBtn === 0 ? "soft" : ""} onClick={() => changeCurrencyType(0)}>
+            N/A
+          </Button>
+          <Button
+            className={selectedBtn === 1 ? constants.filterValuesIds.SELECTED_CURRENCY + " !bg-primary" : " !bg-neutral-100 dark:!bg-neutral-800"} id="1"
+            variant={selectedBtn === 1 ? "soft" : ""} onClick={() => changeCurrencyType(1)}>
+            <StardollarIcon />
+          </Button>
+          <Button
+            className={selectedBtn === 2 ? constants.filterValuesIds.SELECTED_CURRENCY + " !bg-primary" : " !bg-neutral-100 dark:!bg-neutral-800"} id="2"
+            variant={selectedBtn === 2 ? "soft" : ""} onClick={() => changeCurrencyType(2)}>
+            <StarcoinIcon />
+          </Button>
+        </ButtonGroup>
       </FormControl>
+
+      <div className={`flex flex-row space-x-2 ${selectedBtn === 0 ? "hidden" : ""}`}>
+        <FormControl>
+          <FormLabel>Min</FormLabel>
+          <input type="number" id={constants.filterValuesIds.FASHION_MIN_PRICE} min={defaultMin} max={defaultMax}
+            onChange={handleMinInputChange} value={value[0]} className='my-input font-normal h-9 w-[4.5rem] text-clip' />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Max</FormLabel>
+          <input type="number" id={constants.filterValuesIds.FASHION_MAX_PRICE} min={defaultMin} max={defaultMax}
+            onChange={handleMaxInputChange} value={value[1]} className='my-input font-normal h-9 w-[4.5rem]' />
+        </FormControl>
+      </div>
     </div>
   );
 }
