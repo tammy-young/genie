87255db8fb1359aa4303f1b915fde8981dbd530f
Tammy Young <tammy.q.young@gmail.com>
2025-11-30 17:05:27 -0700

    fix: lint errors

--- src/components/SearchPageTemplate.js
+++ src/components/SearchPageTemplate.js
@@ -15,21 +15,15 @@ const SearchPageTemplate = ({
 }) => {
   const {
     isSearching,
-    setIsSearching,
     searchedItems,
-    setSearchedItems,
     brands,
     setBrands,
-    colours,
     setColours,
-    categories,
     setCategories,
     showScrollToTop,
     scrollToTop,
     sortBy,
-    setSortBy,
     sortedItems,
-    setSortedItems,
     userId,
     search
   } = useSearchContext();
